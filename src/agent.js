b1517b3b85fe0b9ca1b14f188b9b2c4b7a22087e
Warren Koch <warrenkoch@gmail.com>
2019-11-29 04:33:22 -0800

    Last minute documentation

--- src/agent.js
+++ src/agent.js
@@ -2,16 +2,18 @@ import superagentPromise from 'superagent-promise';
 import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
+  // trying out this library for this project.  Not a big fan yet
 
 const API_ROOT = 'https://blockchain.info';
-const GENESIS_BLOCK = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'; 
-// local instance of https://github.com/Blockstream/esplora/blob/master/API.md
+const GENESIS_BLOCK = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
+   // hard-coded because I don't think it's gonna get 51% attacked heh
 
 const responseBody = res => res.body;
 
 const requests = {
   get: url =>
     superagent.get(`${API_ROOT}${url}`).then(responseBody),
+  // post / delete / etc go here, normally. Cut them out for brevity.  Not a bad stucture.
 };
 
 const Blocks = {
