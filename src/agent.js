f762c758188b3c32fbe13cc776ed320dbb3ae21f
Warren Koch <warrenkoch@gmail.com>
2019-12-05 19:24:55 -0800

    Added quick fix for CORS policy errors, so block height search works fully

--- src/agent.js
+++ src/agent.js
@@ -4,7 +4,7 @@ import _superagent from 'superagent';
 const superagent = superagentPromise(_superagent, global.Promise);
   // trying out this library for this project.  Not a big fan yet
 
-const API_ROOT = 'https://blockchain.info';
+const API_ROOT = 'https://cors-anywhere.herokuapp.com/https://blockchain.info';
 const GENESIS_BLOCK = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
    // hard-coded because I don't think it's gonna get 51% attacked heh
 
@@ -12,8 +12,11 @@ const responseBody = res => res.body;
 
 const requests = {
   get: url =>
-    superagent.get(`${API_ROOT}${url}`).then(responseBody),
-  // post / delete / etc go here, normally. Cut them out for brevity.  Not a bad stucture.
+    superagent
+      .get(`${API_ROOT}${url}`)
+      .set('Accept', 'application/json')
+      .then(responseBody),
+      // post / delete / etc go here, normally. Cut them out for brevity.  Not a bad stucture.
 };
 
 const Blocks = {
@@ -32,8 +35,9 @@ const Blocks = {
     requests.get(`/latestblock?cors=true`).then(payload => Blocks.get(payload.hash)),
   genesis: () =>
   	Blocks.get(GENESIS_BLOCK),
-  searchHeight: (search) => requests.get(`/block-height/${search}?cors=true`)
-      .then(payload => Blocks.get(payload.blocks[0].hash))
+  searchHeight: (search) => requests
+    .get(`/block-height/${search}?format=json`)
+    .then(payload => Blocks.get(payload.blocks[0].hash))
 };
 
 export default {
